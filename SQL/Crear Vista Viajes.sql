71f3c1f10b7ffbe1a4e3b1cc717f407edc47634a
Belen <belen.lupani@hotmail.com>
2015-10-19 18:01:55 -0300

    Actualizo scripts importacion

---
+++ SQL/Crear Vista Viajes.sql
@@ -0,0 +1,12 @@
+CREATE VIEW viajes_con_ref
+AS
+SELECT DISTINCT m.FechaSalida,
+				m.FechaLLegada,
+				(SELECT AvionID FROM dbo.Avion WHERE Avion_Matricula=m.Aeronave_Matricula) AS AvionRef,
+				m.Ruta_Codigo,
+				(SELECT TipoServicioID FROM dbo.TipoServicio WHERE TipoSer_Nombre=m.Tipo_Servicio) AS TipoServicioRef,
+				m.Fecha_LLegada_Estimada,
+				(SELECT CiudadID FROM dbo.Ciudad WHERE m.Ruta_Ciudad_Destino=Ciudad_Nombre) AS CiudadDestinoRef,
+				(SELECT CiudadID FROM dbo.Ciudad WHERE m.Ruta_Ciudad_Origen=Ciudad_Nombre) AS CiudadOrigenRef
+FROM gd_esquema.Maestra m
+END
