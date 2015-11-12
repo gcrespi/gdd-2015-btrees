f8354171f756a4083fcb17ddc926a083b46bd22e
gcrespi <crespi.gustavo@gmail.com>
2015-11-12 00:52:19 -0300

    Inicio pantalla de decision baja Aeronave

---
+++ AerolineaFrba/Abm Aeronave/DecisionBajaAeronaveForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace AerolineaFrba.Abm_Aeronave
+{
+    partial class DecisionBajaAeronaveForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnReasignar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.Silver;
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(32, 137);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(110, 71);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "Cancelar pasajes y encomiendas";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            // 
+            // btnReasignar
+            // 
+            this.btnReasignar.BackColor = System.Drawing.Color.Silver;
+            this.btnReasignar.FlatAppearance.BorderSize = 0;
+            this.btnReasignar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReasignar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReasignar.Location = new System.Drawing.Point(188, 144);
+            this.btnReasignar.Name = "btnReasignar";
+            this.btnReasignar.Size = new System.Drawing.Size(107, 56);
+            this.btnReasignar.TabIndex = 6;
+            this.btnReasignar.Text = "Reasignar Viaje";
+            this.btnReasignar.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(62, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(332, 17);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "¿Desea cancelar todos los pasajes y encomiendas ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(431, 17);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "del mismo o desea reasignar los viajes a otra aeronave de la flota?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(411, 17);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "La aeronave seleccionada a dar  de baja tiene viajes asignados";
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.BackColor = System.Drawing.Color.Silver;
+            this.btnVolver.FlatAppearance.BorderSize = 0;
+            this.btnVolver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVolver.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolver.Location = new System.Drawing.Point(333, 144);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(107, 56);
+            this.btnVolver.TabIndex = 42;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = false;
+            // 
+            // DecisionBajaAeronaveForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 250);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReasignar);
+            this.Controls.Add(this.btnCancelar);
+            this.Name = "DecisionBajaAeronaveForm";
+            this.Text = "DecisionBajaAeronaveForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.Button btnCancelar;
+        protected System.Windows.Forms.Button btnReasignar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        protected System.Windows.Forms.Button btnVolver;
+    }
+}
